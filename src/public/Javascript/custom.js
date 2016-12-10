215ace6495efcb659ed806424641a73ee8a1f8d8
Reisa Sitohang <reisasitohang@gmail.com>
2016-12-10 16:59:31 +0100

    deleted foundation

--- src/public/Javascript/custom.js
+++ src/public/Javascript/custom.js
@@ -16,35 +16,32 @@ $( document ).ready( ()=> {
 
 		// Get the modal
 	var modal = document.getElementById('myModal');
-
-	// Get the button that opens the modal
-	var btn = document.getElementById("SaveCard");
-
 	// Get the <span> element that closes the modal
 	var span = document.getElementsByClassName("close")[0];
-
 	// When the user clicks the button, open the modal 
-	btn.onclick = function() {
+	$("#SaveCard").click( ()=> {
 	    modal.style.display = "block";
-	}
-
-	// When the user clicks on <span> (x), close the modal
-	span.onclick = function() {
-	    modal.style.display = "none";
-	}
-
-	// When the user clicks anywhere outside of the modal, close it
-	window.onclick = function(event) {
-	    if (event.target == modal) {
-	        modal.style.display = "none";
-	    }
-	}
-	$("#NameBtn").click( () => {
-		console.log("clicked savecard button")
-		let name = $('#NameCardOwner').val()
-		let srcCard = document.getElementById('TheCard').getElementsByTagName('img')[0].src
-		let SavedCard = '<img style="width:100px;height:80px; margin:5px;" src="'+srcCard+'"/>'+name+''
-		console.log("joehoe : "+srcCard)
-		$("#SavedCard").prepend(SavedCard)
 	})
+
+		span.onclick = function() {
+		    	modal.style.display = "none";
+		}
+		// When the user clicks anywhere outside of the modal, close it
+		window.onclick = function(event) {
+		    if (event.target == modal) {
+		        modal.style.display = "none";
+		    }
+		}	
+	$("#NameBtn").click( () => {
+		if ( $('#NameCardOwner').val() ){
+			console.log("clicked savecard button")
+			let name = $('#NameCardOwner').val()
+			let srcCard = document.getElementById('TheCard').getElementsByTagName('img')[0].src
+			let SavedCard = '<img style="width:100px;height:80px; margin:5px;" src="'+srcCard+'"/>'+name+''
+			console.log("joehoe : "+srcCard)
+			$("#SavedCard").prepend(SavedCard)			
+			modal.style.display = "none";
+		}
+	})
+
 })
