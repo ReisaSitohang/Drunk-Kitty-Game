9a98a4b37a41edeea012c637b0b9ba553bf832a4
Reisa Sitohang <reisasitohang@gmail.com>
2016-12-11 21:03:17 +0100

    Add players and they sorta have Id's and glyphicon

--- src/public/Javascript/custom.js
+++ src/public/Javascript/custom.js
@@ -1,4 +1,5 @@
 $( document ).ready( ()=> {
+
 	//This function shows random card
 	let NextCard = ""
 	function AppendRandomCard (){
@@ -12,7 +13,7 @@ $( document ).ready( ()=> {
 			})
 		})		
 	}
-
+	//Run function on click button or car
 	$('#TheCard').click( () => {
 		AppendRandomCard( )
 	})
@@ -20,33 +21,55 @@ $( document ).ready( ()=> {
 		AppendRandomCard( )
 	})
 
-		// Get the modal
-	var modal = document.getElementById('SaveCardModal');
-	// Get the <span> element that closes the modal
-	var span = document.getElementsByClassName("close")[0];
-	// When the user clicks the button, open the modal 
-	$("#SaveCard").click( ()=> {
-	    modal.style.display = "block";
-	})
-		span.onclick = function() {
-		    	modal.style.display = "none";
-		}
-		// When the user clicks anywhere outside of the modal, close it
-		window.onclick = function(event) {
-		    if (event.target == modal) {
-		        modal.style.display = "none";
-		    }
+	//This function adds a player
+	let PlayerCounter = 0
+	function addPlaya () {
+		if ( $('#AddPlayerName').val() ){
+			PlayerCounter = PlayerCounter+1
+			console.log(PlayerCounter)
+			let name = $('#AddPlayerName').val()
+			$('#players').append('<div class="playerStyle" id="Playa'+PlayerCounter+'"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span><span> '+name+'</span><span id="ScorePlayer'+PlayerCounter+'"></span></div>')
+			$('#AddPlayerName').val(" ")
 		}	
-	$("#NameBtn").click( () => {
-		if ( $('#NameCardOwner').val() ){
-			console.log("clicked savecard button")
-			let name = $('#NameCardOwner').val()
-			let srcCard = document.getElementById('TheCard').getElementsByTagName('img')[0].src
-			let SavedCard = '<img style="width:100px;height:80px; margin:5px;" src="'+srcCard+'"/>'+name+''
-			console.log("joehoe : "+srcCard)
-			$("#SavedCard").prepend(SavedCard)			
-			modal.style.display = "none";
+	}
+	//Run function on click button or on keyboard enter
+	$("#AddPlayerBtn").click( () => {
+		addPlaya ()
+	})
+	$("#AddPlayerName").keypress(function(e) {
+		if(e.which == 13) {
+			addPlaya ()
 		}
 	})
+
+
+	// 	// Get the modal
+	// var modal = document.getElementById('SaveCardModal');
+	// // Get the <span> element that closes the modal
+	// var span = document.getElementsByClassName("close")[0];
+	// // When the user clicks the button, open the modal 
+	// $("#SaveCard").click( ()=> {
+	//     modal.style.display = "block";
+	// })
+	// 	span.onclick = function() {
+	// 	    	modal.style.display = "none";
+	// 	}
+	// 	// When the user clicks anywhere outside of the modal, close it
+	// 	window.onclick = function(event) {
+	// 	    if (event.target == modal) {
+	// 	        modal.style.display = "none";
+	// 	    }
+	// 	}	
+	// $("#NameBtn").click( () => {
+	// 	if ( $('#NameCardOwner').val() ){
+	// 		console.log("clicked savecard button")
+	// 		let name = $('#NameCardOwner').val()
+	// 		let srcCard = document.getElementById('TheCard').getElementsByTagName('img')[0].src
+	// 		let SavedCard = '<img style="width:100px;height:80px; margin:5px;" src="'+srcCard+'"/>'+name+''
+	// 		console.log("joehoe : "+srcCard)
+	// 		$("#SavedCard").prepend(SavedCard)			
+	// 		modal.style.display = "none";
+	// 	}
+	// })
 
 })
