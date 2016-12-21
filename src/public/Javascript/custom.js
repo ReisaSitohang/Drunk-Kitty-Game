cf9a97222238cd0f2906bf8d25561604af53931b
Reisa Sitohang <reisasitohang@gmail.com>
2016-12-21 02:20:51 +0100

    added timer backend

--- src/public/Javascript/custom.js
+++ src/public/Javascript/custom.js
@@ -252,6 +252,32 @@ $( document ).ready( ()=> {
 	$( "#emailbtn" ).click( () => {
 		$( "#close" ).trigger( "click" );
 	});
+
+	//Timer stuff
+	$('#timerbtn').click(()=>{
+		function startTimer(duration, display) {
+		    var timer = duration, minutes, seconds;
+		    setInterval(function () {
+		        minutes = parseInt(timer / 60, 10);
+		        seconds = parseInt(timer % 60, 10);
+
+		        minutes = minutes < 10 ? "0" + minutes : minutes;
+		        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+		        display.text(minutes + ":" + seconds);
+
+		        if (--timer < 0) {
+		            timer = duration;
+		        }
+		    }, 1000);
+		}
+		let min = $('#mins').val()
+		jQuery(function ($) {
+		    var Minutes = 60 * min,
+		        display = $('#time');
+		    startTimer(Minutes, display);
+		});
+	})		
 })
 
 
